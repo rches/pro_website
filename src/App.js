8b417d4d2858011d6ec04623c7922e4f5a189f55
Robert Cheseldine <robert.cheseldine@gmail.com>
2019-12-08 22:50:58 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <div className="section hero">
+                <div className="container">
+                    <h1 className="one-half column">
+                        This is where part one about me goes
+                    </h1>
+                    <h4 className="one-half column">
+                        and this is something pretty clever, too.
+                    </h4>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
