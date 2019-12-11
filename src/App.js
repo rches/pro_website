bd71611ada4bfc48333183924c89116c9bdf5bfd
Robert Cheseldine <robert.cheseldine@gmail.com>
2019-12-10 20:44:25 -0500

    updating with full nav functionality, styling and grammar check

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
             id: 1,
             header: "Home",
             copy:
-                "I improve your systems by measurable amounts through a measurable curiosity. I adapt to leadership through experience within months time of being on a project. I kick two-times the butt of a normal software dev by increasing the swing velocity of my foot. Want to know more about me? Click around this intentionally mimimalistic site to see what I'm up to."
+                "I improve your projects by measurable amounts through curiosity and hard work. I prove my value within an organization within a month's time. I kick two-times the butt of a normal software dev by increasing the swing velocity of my foot. Want to know more about me? Click around this intentionally minimalistic site to see what I'm up to."
         };
     }
 
@@ -22,7 +22,7 @@ class App extends React.Component {
                 id: 1,
                 header: "Home",
                 copy:
-                    "I improve your systems by measurable amounts through a measurable curiosity. I adapt to leadership through experience within months time of being on a project. I kick two-times the butt of a normal software dev by increasing the swing velocity of my foot. Want to know more about me? Click around this intentionally mimimalistic site to see what I'm up to."
+                    "I improve your projects by measurable amounts through curiosity and hard work. I prove my value within an organization within a month's time. I kick two-times the butt of a normal software dev by increasing the swing velocity of my foot. Want to know more about me? Click around this intentionally minimalistic site to see what I'm up to."
             });
         } else if (navTo === "projects") {
             this.setState({
@@ -34,22 +34,21 @@ class App extends React.Component {
                         project_title: "WoW Classic News Scrapper",
                         github:
                             "https://github.com/rches/wowclassic-news-scraper",
-                        description:
-                            "I am a big nerd and I liked playing World of Warcraft. Big problems came when I had no time to keep up with the scuttlebutt regarding it. I created a web scraper that scraped the Blizzard forums, logged the top posts, looked at the scores and lastly, analyzed the positive or negative words in each post to try and understand the mood of each post. Spoiler - the posts are mostly negative."
+                        description: `ApifySDK | NodeJS | MongoDB | React - A web scraping tool that parses online forum discussions to discover the latest news and rumors regarding a video game.`
                     },
                     {
                         id: 2,
                         project_title: "Nursing Timer v2.0",
                         github: "https://github.com/rches/nursing_timer_v2",
                         description:
-                            "I'm a little bit sentimental. This project is a rebuild of my very first app, which was a l;abor of love for my wife and my daughter when she was three weeks old. Any new parent will tell you that brainspace is limited during the first month of a new born. This timer is to help with nursing and BMs to report out should the pediatrician have any questions. Save your peace of mind for snuggles."
+                            "React | Firebase - A timer that helps document important details for tired parents. Made with love for my wife and children."
                     },
                     {
                         id: 3,
                         project_title: "Martian Viewer",
                         github: "https://github.com/rches/martianviewer",
                         description:
-                            "When learning to code I set a few milestones for myself. One of those milestones was to work with NASA data. I realized that coding wouldn't be the same as rocket science, but it would still be really cool to parse official data from something outside the atmosphere. This app will show you a still image from the Mars rover when you submit a date. This was my first React app and also had error handling (thanks, Tom Hanks!). Give it a spin, because Mars really is a cool place to explore."
+                            "React | NASA Official REST API - Basic components that make looking through images of Mars easy. Includes error handling."
                     }
                 ]
             });
@@ -57,11 +56,111 @@ class App extends React.Component {
             this.setState({
                 id: 3,
                 header: "Fun Stuff",
-                copy: "FIND OUT ABOUT ME AND THE  FUN STUFF  FUN STUFF  "
+                copy: [
+                    {
+                        id: 1,
+                        title: "Freelance Illustrator and Graphic Designer",
+                        date_range: "2009 - Present",
+                        description: "https://www.behance.net/robertcheseldine"
+                    },
+                    {
+                        id: 2,
+                        title: "Youth Ministry Volunteer",
+                        date_range: "2017 - Present",
+                        description: ""
+                    },
+                    {
+                        id: 3,
+                        title: "Podcaster",
+                        date_range: "2020 - Present",
+                        description: ""
+                    },
+                    {
+                        id: 4,
+                        title:
+                            "Maryland State Top Finalist - Magic: the Gathering",
+                        date_range: "2008",
+                        description: "http://old.starcitygames.com/decks/26973"
+                    }
+                ]
+            });
+        } else if (navTo === "contact") {
+            this.setState({
+                id: 4,
+                header: "Contact",
+                copy: [
+                    {
+                        id: 1,
+                        title: "LinkedIn",
+                        url: "https://www.linkedin.com/in/robertcheseldine/"
+                    },
+                    {
+                        id: 2,
+                        title: "Twitter",
+                        url: "https://twitter.com/rcheseldine1"
+                    },
+                    {
+                        id: 3,
+                        title: "Github",
+                        url: "https://github.com/rches"
+                    },
+                    {
+                        id: 4,
+                        title: "email",
+                        url: "mailto:robert.cheseldine@gmail.com"
+                    }
+                ]
             });
         }
     };
+
     render() {
+        const contentArea = this.state.id;
+        let contentFormat;
+
+        if (contentArea === 1) {
+            contentFormat = <p key={this.state.id}>{this.state.copy}</p>;
+        } else if (contentArea === 2) {
+            const projectList = this.state.copy.map(el => {
+                return (
+                    <li key={el.id}>
+                        <a href={el.github}>{el.project_title}</a>
+                        {" - "}
+                        {el.description}
+                    </li>
+                );
+            });
+
+            contentFormat = <ul>{projectList}</ul>;
+        } else if (contentArea === 3) {
+            const funList = this.state.copy.map(el => {
+                return (
+                    <li key={el.id}>
+                        {el.description ? (
+                            <a href={el.description}>{el.title}</a>
+                        ) : (
+                            el.title
+                        )}
+                        {" - "} {el.date_range}
+                    </li>
+                );
+            });
+
+            contentFormat = <ul>{funList}</ul>;
+        } else if (contentArea === 4) {
+            const contactList = this.state.copy.map(el => {
+                return (
+                    <div key={el.id} className="three columns">
+                        <a href={el.url}>
+                            <button className="button">{el.title}</button>
+                        </a>
+                    </div>
+                );
+            });
+
+            contentFormat = <div className="row">{contactList}</div>;
+        }
+
         return (
             <wrapper className="App">
                 <div className="hero">
@@ -79,7 +178,7 @@ class App extends React.Component {
                                     className="hero-img"
                                 ></img>
                                 <h4 className="hero-description">
-                                    yeah this is really great
+                                    Rob Cheseldine - Software Dev
                                 </h4>
                             </div>
                         </div>
@@ -91,7 +190,6 @@ class App extends React.Component {
                         <div className="row">
                             <div className="one-third column part-a">
                                 <nav className="nav">
-                                    <h3>Nav</h3>
                                     <ul>
                                         <li>
                                             <button
@@ -117,6 +215,14 @@ class App extends React.Component {
                                                 Fun Stuff
                                             </button>
                                         </li>
+                                        <li>
+                                            <button
+                                                value="contact"
+                                                onClick={this.NavClick}
+                                            >
+                                                Contact
+                                            </button>
+                                        </li>
                                     </ul>
                                 </nav>
                             </div>
@@ -127,7 +233,7 @@ class App extends React.Component {
                                     transitionEnterTimeout={150}
                                     transitionLeaveTimeout={75}
                                 >
-                                    <p key={this.state.id}>{this.state.copy}</p>
+                                    {contentFormat}
                                 </CSSTransitionGroup>
                             </div>
                         </div>
